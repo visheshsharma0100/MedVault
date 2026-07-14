aa1464f11207cd813bd51b01f61a92e0f96a4154
Vishesh Sharma <visheshsharma0100@gmail.com>
2026-07-14 06:15:28 +0530

    Update Port

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 // let UserId=1;
 // Signup route
